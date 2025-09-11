1687a9b6ab0566fbdcaccad71350edb81d09f621
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2025-09-11 12:59:56 +0600

    Delete db.py

--- backend/db.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# Add this to app creation
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123@localhost/LetsWrite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
