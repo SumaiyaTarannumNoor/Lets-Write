0233cc586e0c1e10c6b87cd863124f29144a42e5
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2025-09-13 20:41:22 +0600

    Backend deleted

--- backend/app/services/text_service.py
+++
@@ -1,6 +0,0 @@
-from .text_generator import generate_text  # your ML logic
-
-class TextService:
-    @staticmethod
-    def generate(prompt, length=300):
-        return generate_text(prompt, length=length)
